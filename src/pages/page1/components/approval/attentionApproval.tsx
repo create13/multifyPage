79ad30a4777cd99483c19b0021f2529f75d95492
augus <augus.li@coho.com.cn>
2019-04-29 17:22:37 +0800

    组件整理

--- src/pages/page1/components/approval/attentionApproval.tsx
+++ src/pages/page1/components/approval/attentionApproval.tsx
@@ -1,4 +1,63 @@
 import React, { Component } from 'react'
+import { Calendar, Badge } from 'antd';
+function getListData(value:any) {
+  let listData;
+  switch (value.date()) {
+    case 8:
+      listData = [
+        { type: 'warning', content: 'This is warning event.' },
+        { type: 'success', content: 'This is usual event.' },
+      ]; break;
+    case 10:
+      listData = [
+        { type: 'warning', content: 'This is warning event.' },
+        { type: 'success', content: 'This is usual event.' },
+        { type: 'error', content: 'This is error event.' },
+      ]; break;
+    case 15:
+      listData = [
+        { type: 'warning', content: 'This is warning event' },
+        { type: 'success', content: 'This is very long usual event。。....' },
+        { type: 'error', content: 'This is error event 1.' },
+        { type: 'error', content: 'This is error event 2.' },
+        { type: 'error', content: 'This is error event 3.' },
+        { type: 'error', content: 'This is error event 4.' },
+      ]; break;
+    default:
+  }
+  return listData || [];
+}
+
+function dateCellRender(value:any) {
+  const listData = getListData(value);
+  return (
+    <ul className="events">
+      {
+        listData.map((item:any) => (
+          <li key={item.content}>
+            <Badge status={item.type} text={item.content} />
+          </li>
+        ))
+      }
+    </ul>
+  );
+}
+
+function getMonthData(value:any) {
+  if (value.month() === 8) {
+    return 1394;
+  }
+}
+
+function monthCellRender(value:any) {
+  const num = getMonthData(value);
+  return num ? (
+    <div className="notes-month">
+      <section>{num}</section>
+      <span>Backlog number</span>
+    </div>
+  ) : null;
+}
 
 export default class AttentionApproval extends Component {
     constructor(props:any) {
@@ -11,8 +70,10 @@ export default class AttentionApproval extends Component {
     
   render() {
     return (
-      <div>
-        AttentionApproval
+      <div className="attention-approval">
+        {/* <div className="line-left"> */}
+          <Calendar dateCellRender={dateCellRender} monthCellRender={monthCellRender} />
+        {/* </div> */}
       </div>
     )
   }
---
+++ src/pages/page1/components/approval/subordinatesApproval.tsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class subordinatesApproval extends Component {
+    constructor(props:any) {
+      super(props)
+    
+      this.state = {
+         
+      }
+    }
+    
+  render() {
+    return (
+      <div className="subordinates-approval">
+        
+      </div>
+    )
+  }
+}
